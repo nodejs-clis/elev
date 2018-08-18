34d60373827bdf510effc45d98165259406b20c6
#云淡然 <cloudcome@163.com>
2018-08-18 13:51:24 +0800

    ~ 更新 cli 模块

---
+++ src/libs/daemon.js
@@ -0,0 +1,15 @@
+/**
+ * 守护进程
+ * @description 创建可以常驻的子进程用来执行定时任务
+ * @author ydr.me
+ * @create 2018-08-18 13:33
+ * @update 2018-08-18 13:33
+ */
+
+
+'use strict';
+
+var spawn = require('child_process').spawn;
+
+
+
